02f8f717470982acde2b096c5c5878d385024549
nluizsoliveira <nluizsoliveira@gmail.com>
2022-05-31 15:19:54 -0300

    feat(frontend): add html, css, js

---
+++ frontend/public/main.js
@@ -0,0 +1,84 @@
+const validateCodes = function(e){
+    e.preventDefault();
+    const codeInputs = document.getElementsByClassName("codeInput")
+    const [filledCodeValues, filledQty] = getFilledCodeValues(codeInputs)
+
+    alertAllFilledNull(filledQty)
+
+    const email = getEmail()
+    const name = getName()
+
+    if(email && name && filledQty) {
+        sendForm(filledCodeValues, email, name)
+    }
+    return false;
+}
+
+const getFilledCodeValues = function(codeInputs){
+    filledCodeValues = {}
+    let index = 1;
+    for (const codeInput of codeInputs) {
+        if (codeInput.value!== ""){
+            filledCodeValues[index] = codeInput.value
+        }
+        index++;
+    }
+    const filledQty = getDictLen(filledCodeValues) 
+    return [filledCodeValues, filledQty]
+}
+
+const alertAllFilledNull = (filledQty) => {
+    const ERROR_MSG = "ERRO: Preencher ao menos um código."
+    if(!filledQty) {
+        alert(ERROR_MSG)
+    }
+}
+
+
+const getDictLen = (dict) =>{
+    return Object.keys(dict).length 
+}
+
+const getEmail = () => {
+    const email = document.getElementById("email").value
+    return email
+}
+
+const getName = () => {
+    const name = document.getElementById("name").value
+    return name
+}
+
+const sendForm = (filledCodeValues, email, name) =>{
+    const port = '3000'
+    const url = "http://localhost:" + port + "/"
+
+    const data = JSON.stringify(
+        {
+            filledCodeValues,
+            email: email,
+            name: name
+        }
+    )
+
+    fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: data,
+      })
+        .then((res) => res.json())
+        .then((data) => {
+            alertAllFilledInvalid(data.validValuesQty)
+        });
+
+    console.log(data)
+}
+
+const alertAllFilledInvalid = (validValuesQty) => {
+    const ERROR_MSG = "ERRO: Nenhum código está correto."
+    if(!validValuesQty){
+        alert(ERROR_MSG)
+    }
+}
